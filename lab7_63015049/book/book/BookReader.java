df0296c3dcaa9c5aa40856f033939563b0988bd1
Nattaphon Suksomroop <88423052+NattaphonSuksomroop@users.noreply.github.com>
2022-03-19 00:04:11 +0700

    Add files via upload

---
+++ lab7_63015049/book/book/BookReader.java
@@ -0,0 +1,20 @@
+package com.solid.book;
+
+import java.util.List;
+
+public class BookReader implements Print{
+
+    public static void main(String[] args) {
+
+        BookReader bookreader = new BookReader();
+        bookreader.printToScreen(); //Print
+    }
+
+    @Override
+    public void printToScreen() {
+        Book book = new Book("Alive", List.of("Do", "not", "stop", "your", "dream"));
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
+    }
+}
---
+++ lab7_63015049/lsp/lsp/Layout.java
@@ -0,0 +1,5 @@
+package com.solid.lsp;
+
+interface Layout{
+    int getArea(); //Square
+}
